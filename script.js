613c868daaeca0a89f424c99b10fb67973c0a028
Ryan Michael Keller <211752292+averagebagelenjoyer@users.noreply.github.com>
2026-01-07 18:02:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let ip = undefined
+
+fetch('https://api.ipify.org?format=json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then(data => {
+        ip = data.ip;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
 let index = 0
 
 function randomStr(str = String) {
@@ -5,7 +21,7 @@ function randomStr(str = String) {
         A: "Ａ", B: "Ｂ", C: "Ｃ", D: "Ｄ", E: "Ｅ", F: "Ｆ", G: "Ｇ", H: "Ｈ",
         I: "Ｉ", J: "Ｊ", K: "Ｋ", L: "Ｌ", M: "Ｍ", N: "Ｎ", O: "Ｏ", P: "Ｐ",
         Q: "Ｑ", R: "Ｒ", S: "Ｓ", T: "Ｔ", U: "Ｕ", V: "Ｖ", W: "Ｗ", X: "Ｘ",
-        Y: "Ｙ", Z: "Ｚ", " ":  "　"
+        Y: "Ｙ", Z: "Ｚ", " ": "　"
     }
     if (index >= str.length) {
         index = 0
