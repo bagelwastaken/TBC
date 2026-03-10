d80403a3bdcda3935e297b5e4cee1bb532a34cfa
Ryan Michael Keller <211752292+averagebagelenjoyer@users.noreply.github.com>
2026-03-10 10:39:30 -0700

    im bad at summaries

--- script.js
+++ script.js
@@ -10,7 +10,40 @@ function loadHTML(selector, url) {
         .catch(err => console.error("Error loading component:", err));
 }
 
-// Load header and footer
 document.addEventListener("DOMContentLoaded", () => {
     loadHTML("header", "./blank.html");
 });
+
+// Source - https://stackoverflow.com/a/61511955
+// Posted by Yong Wang, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-10, License - CC BY-SA 4.0
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+(async function () {
+    if (
+        document.cookie.split(";").some((item) => item.trim().startsWith("password="))
+    ) {
+        const loginBtn = await waitForElm("#loginBtn");
+        loginBtn.remove();
+    }
+})();
